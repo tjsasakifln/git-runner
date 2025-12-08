0c45637023b7ff2f0c74d59b20834853a26b90b8
Tiago Sasaki <tiago.sasaki@gmail.com>
2025-12-08 11:57:11 -0300

    Update runner description for specific repository

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Runner em nível de organização
+# Runner para repositório específico
 ./config.sh \
-    --url "https://github.com/${GITHUB_OWNER}" \
+    --url "https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}" \
     --token "${GITHUB_TOKEN}" \
     --name "${RUNNER_NAME:-railway-runner}" \
     --work "_work" \
